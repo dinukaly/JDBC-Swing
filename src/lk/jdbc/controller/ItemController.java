2b708978998f945f6a4b96a1be8b0921d6526336
Dinuka Liyanage <infodinukalk@gmail.com>
2025-04-03 14:01:07 +0530

    initial commmit

---
+++ src/lk/jdbc/controller/ItemController.java
@@ -0,0 +1,31 @@
+package lk.jdbc.controller;
+
+import lk.jdbc.dto.ItemDto;
+
+import java.util.ArrayList;
+
+public class ItemController {
+    public String saveItem(ItemDto itemDto){
+        String resp = null;
+        return resp;
+    }
+    public String updateItem(ItemDto itemDto){
+        String resp = null;
+        return resp;
+    }
+
+    public String deleteItem (String code){
+        String resp = null;
+        return resp;
+    }
+
+    public  ItemDto searchItem(String code){
+        ItemDto itemDto = null;
+        return  itemDto;
+    }
+
+    public ArrayList<ItemDto> getAll(){
+        ArrayList<ItemDto> itemDtos = null;
+        return itemDtos;
+    }
+}
