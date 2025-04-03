2701b1d63a9e56ed08a3d73863a5bbfebb025805
Dinuka Liyanage <infodinukalk@gmail.com>
2025-04-03 14:01:19 +0530

    initial commmit

---
+++ src/lk/jdbc/controller/CustomerController.java
@@ -0,0 +1,33 @@
+package lk.jdbc.controller;
+
+import lk.jdbc.dto.CustomerDto;
+
+import java.util.ArrayList;
+
+public class CustomerController {
+
+    public String saveCustomer(CustomerDto customerDto){
+        String resp = null;
+        return resp;
+    }
+
+    public String updateCustomer(CustomerDto customerDto){
+        String resp = null;
+        return resp;
+    }
+
+    public String deleteCustomer(String customerCode){
+        String resp = null;
+        return resp;
+    }
+
+    public CustomerDto searchCustomer(String customerCode){
+        CustomerDto customerDto = null;
+        return  customerDto;
+    }
+
+    public ArrayList<CustomerDto> getAll(){
+        ArrayList<CustomerDto> customerDtos = null;
+        return customerDtos;
+    }
+}
