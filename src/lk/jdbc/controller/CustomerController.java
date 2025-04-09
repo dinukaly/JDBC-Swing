d5525d2da626ae6ea1f834bf9d06cef64b6a5403
Dinuka Liyanage <infodinukalk@gmail.com>
2025-04-09 20:04:08 +0530

    implemented crud methods

--- src/lk/jdbc/controller/CustomerController.java
+++ src/lk/jdbc/controller/CustomerController.java
@@ -3,31 +3,34 @@ package lk.jdbc.controller;
 import lk.jdbc.dto.CustomerDto;
 
 import java.util.ArrayList;
+import lk.jdbc.model.CustomerModel;
 
 public class CustomerController {
+    
+    CustomerModel customerModel = new CustomerModel();
 
     public String saveCustomer(CustomerDto customerDto){
-        String resp = null;
+        String resp = customerModel.saveCustomer(customerDto);
         return resp;
     }
 
     public String updateCustomer(CustomerDto customerDto){
-        String resp = null;
+        String resp = customerModel.updateCustomer(customerDto);
         return resp;
     }
 
     public String deleteCustomer(String customerCode){
-        String resp = null;
+        String resp = customerModel.deleteCustomer(customerCode);
         return resp;
     }
 
     public CustomerDto searchCustomer(String customerCode){
-        CustomerDto customerDto = null;
+        CustomerDto customerDto = customerModel.searchCustomer(customerCode);
         return  customerDto;
     }
 
     public ArrayList<CustomerDto> getAll(){
-        ArrayList<CustomerDto> customerDtos = null;
+        ArrayList<CustomerDto> customerDtos = customerModel.getAllCustomers();
         return customerDtos;
     }
 }
