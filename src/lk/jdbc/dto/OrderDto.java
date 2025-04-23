e8d78f733464e7a71402344dace06d32da834ea1
Dinuka Liyanage <infodinukalk@gmail.com>
2025-04-23 23:19:51 +0530

    done

--- src/lk/jdbc/dto/OrderDto.java
+++ src/lk/jdbc/dto/OrderDto.java
@@ -9,5 +9,46 @@ package lk.jdbc.dto;
  * @author dinuka
  */
 public class OrderDto {
+    private String orderIt;
+    private String date;
+    private String custId;
+
+    public OrderDto() {
+    }
+
+    public OrderDto(String orderIt, String date, String custId) {
+        this.orderIt = orderIt;
+        this.date = date;
+        this.custId = custId;
+    }
+
+    public String getOrderIt() {
+        return orderIt;
+    }
+
+    public void setOrderIt(String orderIt) {
+        this.orderIt = orderIt;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCustId() {
+        return custId;
+    }
+
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDto{" + "orderIt=" + orderIt + ", date=" + date + ", custId=" + custId + '}';
+    }
     
 }
